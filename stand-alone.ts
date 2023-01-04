b0d2b4b38bfaa996ff30c320e784d3efceb0e3cc
Kiichi Takeuchi <kiichitakeuchi@desktop-35t68if.myfiosgateway.com>
2023-01-04 17:31:46 -0500

    path changed

--- stand-alone.ts
+++ stand-alone.ts
@@ -1,5 +1,5 @@
 import { KExport } from 'k-export'
-const kex = new KExport('~/Desktop/kiichi-portfolio/public',// source folder //'~/Library/Mobile Documents/iCloud~md~obsidian/Documents/obsidian-kiichi-portfolio/',
+const kex = new KExport('~/Desktop/kiichi-portfolio/public' ,// source folder //'~/Library/Mobile Documents/iCloud~md~obsidian/Documents/obsidian-kiichi-portfolio/',
             '~/Desktop/kiichi-portfolio/public', // template folder
             '~/Desktop/kiichi-portfolio/public'); // destination folder
 kex.start();
